bced3396c60299edf5458cd2441647e0235b0a2d
marikben <70907667+marikben@users.noreply.github.com>
2021-04-02 16:48:15 +0300

    navigation changes

--- Quiz.js
+++ Quiz.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import{ FlatList, View, Text, StatusBar, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import{ Button, FlatList, View, Text, StatusBar, StyleSheet} from 'react-native';
 
-export default function History() {
-    return (<View><Text>Quiz</Text></View>);
+export default function Quiz() {
+    const [skin, setSkin] = useState('');
+
+    useEffect (() => {
+        console.log(skin)
+    })
+    const active = (param) => {
+        switch(param) {
+ 
+            case 'Oily':
+              setSkin('Oily');
+              break;
+            
+            case 'Dry':
+              setSkin('Dry')
+              break;
+       
+            case 'Acne':
+              setSkin('Acne')
+              break;
+       
+            case 'Normal':
+              setSkin('Normal')
+              break;
+       
+            default:
+              setSkin('');
+          
+            }
+    }
+    return (<View><Text>
+        <Button title='Oily' onPress={() => active('Oily')}></Button>
+        <Button title='Dry' onPress={() => active('Dry')}></Button>
+        <Button title='Normal' onPress={() => active('Normal')}></Button>
+        <Button title='Acne' onPress={() => active('Acne')}></Button>
+
+        </Text></View>);
 }
