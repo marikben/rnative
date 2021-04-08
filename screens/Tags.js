f8f79403a7c366bbfefd0bacf2009c83698ba94f
marikben <70907667+marikben@users.noreply.github.com>
2021-04-08 17:07:32 +0300

    experiencing with tags + switch case

--- screens/Tags.js
+++ screens/Tags.js
@@ -8,6 +8,48 @@ export default function Tags ({ navigation }) {
     const [tags, setTags] = useState([]);
     const type = navigation.getParam('list');
     
+    useEffect(()=>{ 
+        setTags({'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'})
+        console.log(tags)
+    });
+
+    const testi = (param) => {
+    
+    switch(param) {
+        case 'foundation':
+          console.log('1');  
+          break;
+        case 'mascara':
+          console.log('mo')
+          break;
+        case 'lipstick':
+            console.log('hei')
+            break;
+        case 'lipliner':
+            console.log('hhui')
+            break;
+        case 'eyeshadow':
+            console.log('hei')
+            break;
+        case 'eyeliner':
+            console.log('kok')
+            break;
+        case 'bronzer':
+            console.log('yuu')
+            break;
+        case 'blush':
+            console.log('vvv')
+            break;
+        case 'nail_polish':
+            console.log('cv')
+            break;
+
+        default:
+          setTags('toimii');
+      
+        }
+    }
+    
     return(
     <View style={styles.container}>
         <Text>Testing{type}</Text>
