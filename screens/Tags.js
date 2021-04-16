a2c7d6d639454e4e81f5ab3793860bbdab641278
marikben <70907667+marikben@users.noreply.github.com>
2021-04-16 15:48:31 +0300

    tag selection experimenting, tag selector config

--- screens/Tags.js
+++ screens/Tags.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { Button, FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import { Header, Icon, ListItem } from 'react-native-elements';
 import TagSelector from 'react-native-tag-selector';
+import { TagSelect } from 'react-native-tag-select';
 
 export default function Tags ({ navigation }) {
     const [selections, setSelections] = useState([]);
     const type = navigation.getParam('list');
     const name = navigation.getParam('name');
     const tags = getTags(type);
-    
+    const data = [{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+    {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Cruelty free', 'id': 'Cruelty free'}];
+
+
+    console.log(selections)
     useEffect(() => {
         getTags(type) 
     })
@@ -17,47 +22,47 @@ export default function Tags ({ navigation }) {
     
     switch(param) {
         case 'foundation':
-         return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-         {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'No talc', 'id': 'No talc'},
-         {'name': 'Cruelty free', 'id': 'Cruelty free'}, {'name': 'Alcohol free', 'id': 'Alcohol free'},
-         {'name': 'Oil free', 'id': 'Oil free'}, {'name': 'Silicone free', 'id': 'Silicone free'}]
-          break;
-        case 'mascara':
-          return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-          {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
-          {'name': 'Organic', 'id': 'Organic'}]
-          break;
-        case 'lipstick':
             return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Cruelty free', 'id': 'Cruelty free'},
-            {'name': 'Organic', 'id': 'Organic'}]
-            break;
-        case 'lip_liner':
-            return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Cruelty free', 'id': 'Cruelty free'}];
-            break;
-        case 'eyeshadow':
-            return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
-            {'name': 'Organic', 'id': 'Organic'}, {'name': 'No talc', 'id': 'No talc'}]
-            break;
-        case 'eyeliner':
-            return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
-            {'name': 'Organic', 'id': 'Organic'}];
-            break;
-        case 'bronzer':
-            return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Organic', 'id': 'Organic'}]
-            break;
-        case 'blush':
-            return [{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
-            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'},
-            {'name': 'Organic', 'id': 'Organic'}, {'name': 'No talc', 'id': 'No talc'}]
-            break;
-        case 'nail_polish':
-            return([{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'}])
-            break;
+            {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'No talc', 'id': 'No talc'},
+            {'name': 'Cruelty free', 'id': 'Cruelty free'}, {'name': 'Alcohol free', 'id': 'Alcohol free'},
+            {'name': 'Oil free', 'id': 'Oil free'}, {'name': 'Silicone free', 'id': 'Silicone free'}]
+             break;
+           case 'mascara':
+             return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+             {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
+             {'name': 'Organic', 'id': 'Organic'}]
+             break;
+           case 'lipstick':
+               return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Cruelty free', 'id': 'Cruelty free'},
+               {'name': 'Organic', 'id': 'Organic'}]
+               break;
+           case 'lip_liner':
+               return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Cruelty free', 'id': 'Cruelty free'}];
+               break;
+           case 'eyeshadow':
+               return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
+               {'name': 'Organic', 'id': 'Organic'}, {'name': 'No talc', 'id': 'No talc'}]
+               break;
+           case 'eyeliner':
+               return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'}, {'name': 'Ecocert', 'id': 'Ecocert'},
+               {'name': 'Organic', 'id': 'Organic'}];
+               break;
+           case 'bronzer':
+               return[{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Organic', 'id': 'Organic'}]
+               break;
+           case 'blush':
+               return [{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'},
+               {'name': 'Hypoallergenic', 'id': 'Hypoallergenic'},
+               {'name': 'Organic', 'id': 'Organic'}, {'name': 'No talc', 'id': 'No talc'}]
+               break;
+           case 'nail_polish':
+               return([{'name': 'Vegan', 'id': 'Vegan'}, {'name': 'Natural', 'id': 'Natural'}])
+               break;   
 
         default:
           return(['']);
@@ -76,11 +81,17 @@ export default function Tags ({ navigation }) {
     <View style={styles.container}>
        
         <Text style={{padding: 10, fontSize: 16}}>Select preferences for your {name}</Text>
-        <View style={{padding: 20}}>
+        <View style={styles.buttonContainer}>
 				<TagSelector 
-					itemLabelStyle={{color:'red'}}
-                    itemStyleSelected={{color:'#FFFFFF'}}
 					tags={tags}
+                    //containerStyle = {styles.container}
+                    selectedTagStyle = {styles.itemSelected}
+                    tagStyle = {styles.item}
+                    expandTextStyle={styles.label}
+                    //separatorStyle = {styles.separator}
+                    //expandBtnStyle = {styles.btnStyle}
+                    //expandTextStyle = {styles.btnText}
+  
 					onChange={(selected) => setSelections({ selected })} />
 			</View>
             <Button color='#E35D86' onPress={() => navigation.navigate('Results', {'list':selections, 'type':type})} title="Show products"></Button>
@@ -98,5 +109,39 @@ const styles = StyleSheet.create({
         header: {
           //paddingTop: 40,
           backgroundColor: 'whitesmoke',
-        }
+        },
+        buttonContainer: {
+            padding: 15,
+          },
+          buttonInner: {
+            marginBottom: 15,
+          },
+          labelText: {
+            color: '#333',
+            fontSize: 15,
+            fontWeight: '500',
+            marginBottom: 15,
+          },
+          item: {
+            borderWidth: 0.25,
+            borderColor: '#999999',  
+            borderRadius: 5,  
+            backgroundColor: '#FFF',
+            padding: 7,
+            margin: 2
+          },
+          label: {
+            color: '#333'
+          },
+          itemSelected: {
+            borderWidth: 0.25,
+            borderColor: '#999999',  
+            borderRadius: 5, 
+            backgroundColor: '#dddddd',
+            padding: 7,
+            margin: 2
+          },
+          labelSelected: {
+            color: '#FFF',
+          },
     })
