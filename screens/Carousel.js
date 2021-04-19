0193a2a9fb1b60b223b045fe7d9fdcca46c12fc4
marikben <70907667+marikben@users.noreply.github.com>
2021-04-19 12:47:32 +0300

    username visibility

--- screens/Carousel.js
+++ screens/Carousel.js
@@ -1,66 +1,20 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import firebase from '../database/firebaseDB';
 import {Dimensions, View, ImageBackground, FlatList, Text, TouchableWithoutFeedback, StatusBar} from 'react-native';
 import {Card} from '../components/Card'
 import {Title} from '../components/Title'
 import Header from '../components/Header.js'
 
-//A way approved in the Firebase documentation to get user details 
-//(firebase.auth().currentUser.displayName would crash)
-var user = firebase.auth().currentUser;
-var name;
-
-if (user != null) {
-  name = user.displayName;
-}
-
-const eventslist = [
-  
-  { 
-    src:
-      'https://www.teads.com/wp-content/uploads/2019/10/cover-research-beauty.jpg',
-    title: 'Welcome',
-    description: <Text>
-    Hello, {name}! {'\n'}
-    Swipe left to learn more about how to use this app.
-  </Text>
-  },
-  {
-    src:
-      'https://media.self.com/photos/57e00e471db118765d302bdd/2:1/pass/sub-channel-beauty_makeup.jpg',
-    title: 'Brunch',
-    description: 'Bää'
-  },
-  {
-    src:
-      'https://media-we-cdn.oriflame.com/-/media/Images/Navigation/Main-menu/Discover-2020/Beauty-by-sweden/13370393_2732x778x580.ashx?u=0101010000',
-    title: 'Lunch',
-    description: 'Böö'
-  },
-  {
-    src:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfWQzqNelD6MSJgPKigH22LtCosXeK4d9W1QWvfknbN-hFPNXssY9U-Ly9VkGatsiXw5M&usqp=CAU',
-    title: 'Snacks',
-    description: 'Zii'
-  },
-  {
-    src:
-      'https://media.allure.com/photos/5f6fa448eb43f61579aac592/16:9/w_3408,h_1917,c_limit/coconut-oil-uses-lede.jpg',
-    title: 'Dinner',
-    description: 'BZäp'
-  },
-];
-
-export default class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
+export default class Carousel extends Component {
+  constructor() {
+    super();
 
     this.state = {
       data: '',
-      displayName: '',
+      uid: '',
     };
   }
-
+  
   keyExtractor = (item, index) => index.toString();
   renderCarousel = ({item}) => (
     <Card style={styles.cardContainerStyle}>
@@ -84,7 +38,48 @@ export default class Carousel extends React.Component {
   );
 
   render() {
-    
+    this.state = { 
+      displayName: firebase.auth().currentUser.displayName,
+      uid: firebase.auth().currentUser.uid
+    }
+
+    const eventslist = [
+  
+      { 
+        src:
+          'https://www.teads.com/wp-content/uploads/2019/10/cover-research-beauty.jpg',
+        title: 'Welcome',
+        description: <Text>
+        Hello, {this.state.displayName}! {'\n'}
+        Swipe left to learn more about how to use this app.
+      </Text>
+      },
+      {
+        src:
+          'https://media.self.com/photos/57e00e471db118765d302bdd/2:1/pass/sub-channel-beauty_makeup.jpg',
+        title: 'Brunch',
+        description: 'Bää'
+      },
+      {
+        src:
+          'https://media-we-cdn.oriflame.com/-/media/Images/Navigation/Main-menu/Discover-2020/Beauty-by-sweden/13370393_2732x778x580.ashx?u=0101010000',
+        title: 'Lunch',
+        description: 'Böö'
+      },
+      {
+        src:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfWQzqNelD6MSJgPKigH22LtCosXeK4d9W1QWvfknbN-hFPNXssY9U-Ly9VkGatsiXw5M&usqp=CAU',
+        title: 'Snacks',
+        description: 'Zii'
+      },
+      {
+        src:
+          'https://media.allure.com/photos/5f6fa448eb43f61579aac592/16:9/w_3408,h_1917,c_limit/coconut-oil-uses-lede.jpg',
+        title: 'Dinner',
+        description: 'BZäp'
+      },
+    ];
+
     return (
      <View style={styles.mainContainer}>
     <Header />
