8688ae583a366c75870812276ce0807bf912fb85
marikben <70907667+marikben@users.noreply.github.com>
2021-04-29 10:17:54 +0300

    dropdown picker

--- components/UserSettings.js
+++ components/UserSettings.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, FlatList, Image, Modal, Pressable, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
+import { Alert, Button, FlatList, Image, Modal, Picker, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
 import firebase from '../database/firebaseDB';
-import Header from '../components/Header.js'
+import Header from './Header.js'
 import { DrawerItems } from 'react-navigation-drawer';
 
-export default function Favourites () {
+export default function UserSettings () {
+    const [selectedValue, setSelectedValue] = useState("java");
     return(<View>
-        <Text>Favourites</Text>
+         <View >
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+    </View>
     </View>)
 }
