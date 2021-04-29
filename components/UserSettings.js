d2f1059293f44ad4fdd8db8cc4daa2f0a3e3f59e
marikben <70907667+marikben@users.noreply.github.com>
2021-04-29 10:32:43 +0300

    dropdown picker

--- components/UserSettings.js
+++ components/UserSettings.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, FlatList, Image, Modal, Picker, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
+import { Alert, Button, FlatList, Image, Modal, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
 import firebase from '../database/firebaseDB';
 import Header from './Header.js'
-import { DrawerItems } from 'react-navigation-drawer';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function UserSettings () {
     const [selectedValue, setSelectedValue] = useState("java");
+    console.log(selectedValue)
+    const [questions, setQuestions] = useState([{label: 'Base products', value: 'foundation'}, 
+    {label: 'Bronzer', value: 'bronzer'}, {label: 'Blush', value: 'blush'},
+    {label: 'Brows', value: 'eyebrow'}, {label: 'Eyeliner', value: 'eyeliner'},
+    {label: 'Eyeshadow', value: 'eyeshadow'}, {label: 'Lipliner', value: 'lip_liner'}, 
+    {label: 'Lipstick', value: 'lipstick'}, {label: 'Mascara', value: 'mascara'},
+    {label: 'Nail polish', value: 'nail_polish'}])
     return(<View>
          <View >
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-      >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
-      </Picker>
+         <DropDownPicker
+    items={questions}
+    defaultIndex={0}
+    containerStyle={{width: 150, height: 40}}
+    onChangeItem={item => setSelectedValue(item)}
+/>
     </View>
     </View>)
 }
