e9dacb9596a7d35864c1e388ca7ce9ef3b023e8d
marikben <70907667+marikben@users.noreply.github.com>
2021-05-04 19:39:52 +0300

    user deletion configuration w firebase

--- components/UserSettings.js
+++ components/UserSettings.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, FlatList, Image, Modal, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
+import { Alert, Button, FlatList, Image, Modal, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
 import firebase from '../database/firebaseDB';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function UserSettings () {
-    const [selectedValue, setSelectedValue] = useState("java");
-    console.log(selectedValue)
-    const [questions, setQuestions] = useState([{label: 'Base products', value: 'foundation'}, 
-    {label: 'Bronzer', value: 'bronzer'}, {label: 'Blush', value: 'blush'},
-    {label: 'Brows', value: 'eyebrow'}, {label: 'Eyeliner', value: 'eyeliner'},
-    {label: 'Eyeshadow', value: 'eyeshadow'}, {label: 'Lipliner', value: 'lip_liner'}, 
-    {label: 'Lipstick', value: 'lipstick'}, {label: 'Mascara', value: 'mascara'},
-    {label: 'Nail polish', value: 'nail_polish'}])
-    return(
-         <Button title='delete user' onClick={() => Alert.alert('Are you sure?')} color='#ff0000'></Button>
+    const createTwoButtonAlert = () => {
+        console.log('byy')
+        Alert.alert(
+        "Are you sure?",
+        "All of your data will be permantently lost",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log('bää'),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log('Login') }
+        ],
+        { cancelable: false }
+      );
+       
+    }
+   
+    return(<View>
+        <Text>Byy</Text>
+        
+        <Button title='delete user' onPress={() => createTwoButtonAlert()} color='#ff0000'></Button>
+            
+    </View>
+         
     )
 }
