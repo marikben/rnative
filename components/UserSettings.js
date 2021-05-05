ab43161945f45b7068eff667a4e88ff84bd3cee6
marikben <70907667+marikben@users.noreply.github.com>
2021-05-05 12:55:06 +0300

    profile pic init expo

--- components/UserSettings.js
+++ components/UserSettings.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, Button, FlatList, Image, Modal, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import { Icon, ListItem } from 'react-native-elements';
 import firebase from '../database/firebaseDB';
-import DropDownPicker from 'react-native-dropdown-picker';
 import { withNavigation } from 'react-navigation';
-
+import ImagePick from './ImagePick';
 
 class UserSettings extends React.Component {
 
@@ -36,7 +34,7 @@ class UserSettings extends React.Component {
         <Text>Byy</Text>
         
         <Button title='delete user' onPress={() => createTwoButtonAlert()} color='#ff0000'></Button>
-            
+        <ImagePick />   
     </View>
          
     )
