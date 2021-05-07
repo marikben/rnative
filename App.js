0b638bfafcc7853de2262b4579fc27127521888e
marikben <70907667+marikben@users.noreply.github.com>
2021-05-07 11:12:54 +0300

    logbox for irrelevant warnings

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 //Pull in AppNavigator from the navigation folder
 import AppNavigator from './navigation/AppNavigator'
+import { LogBox } from 'react-native';
 
 export default function App() {
+  //Ignoring "irrelevant" warnings 
+  LogBox.ignoreLogs(['Setting a timer']);
+  LogBox.ignoreLogs(['Your project is accessing']);
   return (
     <AppNavigator /> 
   );
