4d60ca324dd124a5e16c5de0a6441b54cef0c368
marikben <70907667+marikben@users.noreply.github.com>
2021-05-07 19:05:30 +0300

    calendar progress

--- components/CalendarAct.js
+++ components/CalendarAct.js
@@ -2,39 +2,36 @@ import React, { Component, useState } from 'react';
 import { Text, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
-export default class CalendarView extends React.Component {
-   
-      constructor() {
-        super();
-        this.state = { 
-            markedDates: {
-                '2019-07-22': { startingDay: true, endingDay: true, marked: true, color: 'green', },
-              },
-           
-        }
-  
-      }
-      
-    render(){
-        const {moi} = this.state
+export default function CalendarView (){
+    const [select, setSelect] = useState('')
+    const [waterObject, setWaterObject] = React.useState({});
+    const [selected, setSelected] = React.useState(null);
+    const [marked, setMarked] = useState ([])
+
         const selectedDate = (day) => {
-            
-            return day
-            
+            console.log(day)
+            setMarked([...marked, {[day]: {selected: true}}])
+            console.log(marked)
         }
-    console.log(Object.keys(this.state.markedDates))  
+      
     return(<View>
         <Calendar
+        theme={{
+            calendarBackground: '#131A26',
+            textSectionTitleColor: '#ffffff',
+            selectedDayTextColor: '#ffffff',
+            selectedDayBackgroundColor: '#2176FF',
+            dayTextColor: '#ffffff',
+            monthTextColor: '#ffffff',
+            textMonthFontWeight: 'bold',
+        }}
         onDayPress={(day) => {
-            selectedDate(day.day)
-            this.setState({
-              markedDates: {
-                [day.dateString]: { startingDay: true, endingDay: true, marked: true, color: 'pink', },
-              }
-            })
-          }}
-          markingType={'period'}
-          markedDates={this.state.markedDates}/>
-          <Text>Moi {selectedDate(Object.keys(this.state.markedDates))} </Text>
+                setSelected(day["dateString"])
+                selectedDate(day["dateString"])
+            
+        }}
+        markedDates={{ [selected]: {selected: true, selectedColor: '#81c5fe'}}}
+          />
+          <Text>Moi {select}</Text>
     </View>)
-}}
+}
